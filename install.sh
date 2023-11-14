27abcf5e0115f87d9f6d0565a460a9623bafdb25
jinx <xjin@mail.nankai.edu.cn>
2023-11-14 14:29:33 +0800

    [UPDATE] update LibRaw from 0.19.1 to 0.21.1 for supporting more recent cameras.

--- install.sh
+++ install.sh
@@ -10,18 +10,21 @@ python setup.py develop
 ### install rawpy and LibRaw for benchmark
 mkdir -p downloads/
 # download LibRaw and rawpy
-python scripts/download_gdrive.py --id 1B4gJYe3h4UxWTMZzNL2xVodSDUlNbeAR --save-path downloads/LibRaw-0.19.1.zip
+# python scripts/download_gdrive.py --id 1B4gJYe3h4UxWTMZzNL2xVodSDUlNbeAR --save-path downloads/LibRaw-0.19.1.zip
+wget https://www.libraw.org/data/LibRaw-0.21.1.zip -O downloads/LibRaw-0.21.1.zip
 python scripts/download_gdrive.py --id 1EuJsbZ_a_YJHHcGAVA9TXXPnGU90QoP4 --save-path downloads/rawpy.zip
 
 unzip downloads/rawpy.zip -d downloads/
-unzip downloads/LibRaw-0.19.1.zip -d downloads/
+# unzip downloads/LibRaw-0.19.1.zip -d downloads/
+unzip downloads/LibRaw-0.21.1.zip -d downloads/
 
 # setting LibRAW
-cd downloads/LibRaw-0.19.1
+# cd downloads/LibRaw-0.19.1
+cd downloads/LibRaw-0.21.1
 ./configure
 make
 sudo make install
 
 # setting rawpy
 cd ../rawpy
-pip install -e .
+RAWPY_USE_SYSTEM_LIBRAW=1 pip install -e .
